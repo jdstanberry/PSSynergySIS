e24dd703757476f8c7f52738c1f67a25802c6212
Justin Stanberry <jdstanberry@pvesd.org>
2019-09-03 15:46:39 -0700

    Update import to use RosterObject property names

--- functions/New-SynergyDataImport.ps1
+++ functions/New-SynergyDataImport.ps1
@@ -25,42 +25,42 @@ function New-SynergyDataImport {
         [string]$InputFormat,
 
         # OverwriteFlag
-        $OverwriteFlag, 
-        
+        $OverwriteFlag,
+
         # SchoolCode
         $SchoolCode,
-        
+
         # SchoolYear
         $SchoolYear
     )
 
     switch ($InputFormat) {
         'StudentData' {
-            [xml]$ImportXML = 
+            [xml]$ImportXML =
             @"
  <ROOT OVERWRITE_STUDENT="$overwriteFlag" DEBUG="1" >
- $( $ImportObject | ForEach-Object { '<STUDENT LAST_NAME="{0}" FIRST_NAME="{1}" SIS_NUMBER="{2}" E_MAIL="{3}" />' -f $_.last_name, $_.first_name, $_.student_id, $_.NewEmail }
+ $( $ImportObject | ForEach-Object { '<STUDENT LAST_NAME="{0}" FIRST_NAME="{1}" SIS_NUMBER="{2}" E_MAIL="{3}" />' -f $_.LastName, $_.FirstName, $_.SISID, $_.Email }
  )
  </ROOT>
 "@
             Return $ImportXML
-     
+
         }
         'StudentBus' {
-            [xml]$xmlData = 
+            [xml]$xmlData =
             @"
 <?xml version="1.0" encoding="UTF-8"?><ROOT>
-$($ImportObject | ForEach-Object { '<STUDENT_SCHOOL_YEAR SIS_NUMBER="{0}" SCHOOL_CODE="{1}" SCHOOL_YEAR="{2}" BUS_TO_SCHOOL="{3}" />' -f $_.'Perm ID', $SchoolCode, $SchoolYear, [System.Security.SecurityElement]::Escape($_.BusRoute) } 
+$($ImportObject | ForEach-Object { '<STUDENT_SCHOOL_YEAR SIS_NUMBER="{0}" SCHOOL_CODE="{1}" SCHOOL_YEAR="{2}" BUS_TO_SCHOOL="{3}" />' -f $_.'Perm ID', $SchoolCode, $SchoolYear, [System.Security.SecurityElement]::Escape($_.BusRoute) }
 )
 </ROOT>
 "@
             Return $xmlData
-     
-     
-     
+
+
+
         }
         'GridCodes' {
-            [xml]$xmlData = 
+            [xml]$xmlData =
             @"
 <ROOT>
 $( $importObject | ForEach-Object {
