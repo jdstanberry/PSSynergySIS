a77e65dee6e8d95c7d351664ef304a51fbe8af3f
Justin Stanberry <jdstanberry@pvesd.org>
2020-03-10 14:05:38 -0700

    restructure for continuous delivery

---
+++ src/Tests/Unit/PSSynergySIS-Function.Tests.ps1
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------
+Set-Location -Path $PSScriptRoot
+#-------------------------------------------------------------------------
+$ModuleName = 'PSSynergySIS'
+$PathToManifest = [System.IO.Path]::Combine('..', '..', $ModuleName, "$ModuleName.psd1")
+#-------------------------------------------------------------------------
+if (Get-Module -Name $ModuleName -ErrorAction 'SilentlyContinue') {
+    #if the module is already in memory, remove it
+    Remove-Module -Name $ModuleName -Force
+}
+Import-Module $PathToManifest -Force
+#-------------------------------------------------------------------------
+$WarningPreference = "SilentlyContinue"
+#-------------------------------------------------------------------------
+#Import-Module $moduleNamePath -Force
+
+InModuleScope 'PSSynergySIS' {
+    #-------------------------------------------------------------------------
+    $WarningPreference = "SilentlyContinue"
+    #-------------------------------------------------------------------------
+    Describe 'PSSynergySIS Private Function Tests' -Tag Unit {
+        Context 'FunctionName' {
+            <#
+            It 'should ...' {
+
+            }#it
+            #>
+        }#context_FunctionName
+    }#describe_PrivateFunctions
+    Describe 'PSSynergySIS Public Function Tests' -Tag Unit {
+        Context 'FunctionName' {
+            <#
+                It 'should ...' {
+
+                }#it
+                #>
+        }#context_FunctionName
+    }#describe_testFunctions
+}#inModule
