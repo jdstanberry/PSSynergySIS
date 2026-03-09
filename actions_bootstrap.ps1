de0aec91ac8a569a728170aabbd8ac4281c59146
Justin Stanberry <jdstanberrry@pvesd.org>
2026-03-09 09:05:58 -0700

    feat: Add Invoke-Build script for module automation and unpin specific module versions in the bootstrap script.

--- actions_bootstrap.ps1
+++ actions_bootstrap.ps1
@@ -12,23 +12,23 @@ $modulesToInstall = [System.Collections.ArrayList]::new()
 # https://github.com/pester/Pester
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'Pester'
-            ModuleVersion = '5.2.2'
+            # ModuleVersion = '5.2.2'
         }))
 # https://github.com/nightroman/Invoke-Build
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'InvokeBuild'
-            ModuleVersion = '5.8.0'
+            # ModuleVersion = '5.8.0'
         }))
 # https://github.com/PowerShell/PSScriptAnalyzer
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'PSScriptAnalyzer'
-            ModuleVersion = '1.19.1'
+            # ModuleVersion = '1.19.1'
         }))
 # https://github.com/PowerShell/platyPS
 # older version used due to: https://github.com/PowerShell/platyPS/issues/457
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'platyPS'
-            ModuleVersion = '0.12.0'
+            # ModuleVersion = '0.12.0'
         }))
 
 
